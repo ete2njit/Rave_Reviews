b980bb52a0088d5b69c7a4a08b369aa887ecfc88
ete2njit <ete2@njit.edu>
2021-03-08 19:38:46 -0500

    Set up mock frontend to host socket communication

--- scripts/App.js
+++ scripts/App.js
@@ -1,13 +1,61 @@
 import React from "react"
 import Home from "./components/Home"
+import Socket from "./Socket";
 import Movies from "./components/Movies"
 import Error from "./components/Error"
 
 const App= () =>{
+
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const [searchCategory, setSearchCategory] = React.useState("");
+
+    function handlePost(e) {
+        e.preventDefault();
+
+        Socket.emit("search request", {
+            category: searchCategory,
+            searchTerm: searchTerm,
+        });
+      }
+
+
+
+    React.useEffect(() => {
+    Socket.on("connected", (data) => {
+        alert(data["test"]);
+        });
+    }, []);
+
     return (
         <main>
             <Home/>
+
+            <form htmlFor="newitem" onSubmit={handlePost}>
+              <label htmlFor="textbox">
+                <input
+                  type="text"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </label>
+              <label htmlFor="textbox">
+                <input
+                  type="text"
+                  value={searchCategory}
+                  onChange={(e) => setSearchCategory(e.target.value)}
+                />
+              </label>
+              <button
+                className="submit-button"
+                onClick={handlePost}
+                variant="primary"
+                type="submit"
+                value="Submit"
+              >
+                Submit
+              </button>
+            </form>
         </main>
     )
 }
-export default App 
+export default App
