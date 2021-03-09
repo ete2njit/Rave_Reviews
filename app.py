65ee6740a78d9564a81df59ef941c82b23293d2c
rpasricha45 <rpasricha45@gmail.com>
2021-03-08 20:19:08 -0500

    added bootstrap css for local react testing

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def index():
 if __name__ == '__main__':
     SOCKETIO.run(
         APP,
-        host=os.getenv('IP', '0.0.0.0'),
+        host=os.getenv('IP', 'localhost'),
         port=int(os.getenv('PORT', 8080)),
         debug=True
     )
