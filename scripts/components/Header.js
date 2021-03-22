42bce079f269cf9edaa4adcae90f542e9266c484
rpasricha45 <rpasricha45@gmail.com>
2021-03-22 18:44:26 -0400

    added routing

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -7,10 +7,10 @@ const Header= () =>{
       <Navbar bg="primary" variant="dark">
       <Navbar.Brand href="#home">Rave Reviews </Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="Home">Home</Nav.Link>
-        <Nav.Link href="Movies">Movies</Nav.Link>
-        <Nav.Link href="TV Shows">TV Shows</Nav.Link>
-        <Nav.Link href="Books">Books</Nav.Link>
+        <Nav.Link href="#/Home">Home</Nav.Link>
+        <Nav.Link href="#/Movies">Movies</Nav.Link>
+        <Nav.Link href="#/TVShows">TV Shows</Nav.Link>
+        <Nav.Link href="#/Books">Books</Nav.Link>
       </Nav>
     </Navbar>
         
